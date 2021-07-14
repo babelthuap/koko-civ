673b6b20eb9203635021706bce90820a9f1c7659
Nicholas Neumann-Chun <babelthuap@users.noreply.github.com>
2021-07-14 13:57:21 -0700

    Constrain viewport parameters

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import {createCanvas} from './Canvas.js';
 import {HexGrid} from './Grid.js';
 import {clamp, mod} from './util.js';
 
+console.time('initial render');
 let canvas = createCanvas(window.innerWidth, window.innerHeight);
 let hexGrid = new HexGrid(10, 25);
 const viewport = {
@@ -10,35 +11,43 @@ const viewport = {
   scale: 120,
 };
 
-console.time('initial render');
 canvas.attachToDom(document.body);
 hexGrid.render(viewport, canvas);
 console.timeEnd('initial render');
 
+// constrain viewport parameters
+function updateViewport({leftX, topY, scale}) {
+  viewport.leftX = mod(leftX, hexGrid.width);
+  viewport.scale = clamp(scale, canvas.height / hexGrid.height, 300);
+  viewport.topY =
+      clamp(topY, 0, hexGrid.height - canvas.height / viewport.scale);
+  hexGrid.render(viewport, canvas);
+}
+
 // handle window resize
 window.addEventListener('resize', () => {
   canvas.updateDimensions(window.innerWidth, window.innerHeight);
-  hexGrid.render(viewport, canvas);
+  updateViewport(viewport);
 });
 
 // zoom map
 document.addEventListener('wheel', e => {
-  viewport.scale = clamp(
-      viewport.scale - e.deltaY * 0.25, canvas.height / hexGrid.height, 300);
-  viewport.topY =
-      clamp(viewport.topY, 0, hexGrid.height - canvas.height / viewport.scale);
-  hexGrid.render(viewport, canvas);
+  updateViewport({
+    leftX: viewport.leftX,
+    topY: viewport.topY,
+    scale: viewport.scale - e.deltaY * 0.25,
+  });
 });
 
 // drag map
 let dragging = false;
 let startDragX, startDragY;
-canvas.addEventListener('mousedown', ({layerX, layerY}) => {
+document.addEventListener('mousedown', ({layerX, layerY}) => {
   dragging = true;
   startDragX = layerX;
   startDragY = layerY;
 });
-canvas.addEventListener('mousemove', ({layerX, layerY}) => {
+document.addEventListener('mousemove', ({layerX, layerY}) => {
   if (dragging) {
     const dx = (layerX - startDragX) / viewport.scale;
     const dy = (layerY - startDragY) / viewport.scale;
@@ -53,17 +62,24 @@ canvas.addEventListener('mousemove', ({layerX, layerY}) => {
         canvas);
   }
 });
-canvas.addEventListener('mouseup', ({layerX, layerY}) => {
-  dragging = false;
-  const dx = (layerX - startDragX) / viewport.scale;
-  const dy = (layerY - startDragY) / viewport.scale;
-  viewport.leftX = mod(viewport.leftX - dx, hexGrid.width);
-  viewport.topY = clamp(
-      viewport.topY - dy, 0, hexGrid.height - canvas.height / viewport.scale);
-});
+function finalizeDragging({layerX, layerY}) {
+  if (dragging) {
+    console.log('finalizeDragging');
+    dragging = false;
+    const dx = (layerX - startDragX) / viewport.scale;
+    const dy = (layerY - startDragY) / viewport.scale;
+    updateViewport({
+      leftX: viewport.leftX - dx,
+      topY: viewport.topY - dy,
+      scale: viewport.scale,
+    });
+  }
+}
+document.addEventListener('mouseup', finalizeDragging);
+document.addEventListener('mouseleave', finalizeDragging);
 
 // right click
-canvas.addEventListener('contextmenu', e => {
+document.addEventListener('contextmenu', e => {
   e.preventDefault();
   hexGrid.invertColor(
       e.layerX / viewport.scale + viewport.leftX,
