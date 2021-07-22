0f3a475e57c43f33b5fa6d699ea511060219c16a
Nicholas Neumann-Chun <babelthuap@users.noreply.github.com>
2021-07-21 18:06:22 -0700

    Fancy UI box for map editor controls

--- map-editor.js
+++ map-editor.js
@@ -1,10 +1,12 @@
 import board from './js/gameboard.js';
+import {clamp, limitOncePerFrame} from './js/util.js';
 import archipelago from './map-scripts/archipelago.js';
 import continents from './map-scripts/continents.js';
 
 board.init({width: 100, height: 130});
 continents(board, 0.25);
 
+// Handle key presses.
 document.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'a':
@@ -18,11 +20,13 @@ document.addEventListener('keydown', (event) => {
   }
 });
 
+// Handle button clicks.
 document.getElementById('archipelago')
     .addEventListener('click', () => archipelago(board, 0.2));
 document.getElementById('continents')
     .addEventListener('click', () => continents(board, 0.25));
 
+// Handle tile clicks.
 board.addClickListener((event, tile) => {
   switch (event.button) {
     case 0:
@@ -34,6 +38,58 @@ board.addClickListener((event, tile) => {
       // Right click.
       alert(tile.terrain);
       break;
+  }
+});
+
+// Handle click-and-drag controls.
+const CONTROLS = document.getElementById('controls');
+const DRAGBAR = CONTROLS.querySelector('#dragbar');
+let mousePressed = false;
+let dragging = false;
+let controlsBox;
+let startDrag = {};
+DRAGBAR.addEventListener('mousedown', ({clientX, clientY}) => {
+  mousePressed = true;
+  controlsBox = CONTROLS.getBoundingClientRect();
+  startDrag = {clientX, clientY};
+  DRAGBAR.style.cursor = 'grabbing';
+});
+const repositionControls = (clientX, clientY) => {
+  dragging = true;
+  const dx = clientX - startDrag.clientX;
+  const dy = clientY - startDrag.clientY;
+  const {left, width, top, height} = controlsBox;
+  CONTROLS.style.left = `${
+      clamp(
+          controlsBox.left + dx, 0, window.innerWidth - controlsBox.width)}px`;
+  CONTROLS.style.top = `${
+      clamp(
+          controlsBox.top + dy, 0, window.innerHeight - controlsBox.height)}px`;
+};
+document.addEventListener('mousemove', ({clientX, clientY}) => {
+  if (mousePressed) {
+    limitOncePerFrame(repositionControls, clientX, clientY);
+  }
+});
+document.addEventListener('mouseup', () => {
+  mousePressed = false;
+  dragging = false;
+  DRAGBAR.style.cursor = '';
+});
+
+// Handle minimizing controls
+const MINIMIZE = CONTROLS.querySelector('#minimize');
+const PLUS = MINIMIZE.querySelector('.plus');
+const CONTENT = CONTROLS.querySelector('.content');
+let isMinimized = false;
+MINIMIZE.addEventListener('mousedown', () => {
+  isMinimized = !isMinimized;
+  if (isMinimized) {
+    CONTENT.style.display = 'none';
+    PLUS.style.display = '';
+  } else {
+    CONTENT.style.display = '';
+    PLUS.style.display = 'none';
   }
 });
 
