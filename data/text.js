c81b13e60cea9061ce8c7da6c09ec7c5f47078ea
Nicholas Neumann-Chun <babelthuap@users.noreply.github.com>
2021-07-25 11:41:21 -0700

    Text update

--- data/text.js
+++ data/text.js
@@ -1,4 +1,4 @@
 export default {
   TURN_ONE_INTRO:
-      'The sun dawns on the year 10,000 BC!\n\nSince time immemorial, your ancestors have lived as nomadic hunter-gatherers. However, your people have discovered a land of abundance and begun to develop agriculture. They wish to settle down. They look to their leader... Where will you guide them?',
+      'The sun dawns on the year 10,000 BC!\n\nSince time immemorial, your ancestors have lived as nomadic hunter-gatherers. However, your people have discovered a land of abundance and begun to develop agriculture. Now they wish to settle down. They look to their leader... Where will you guide them?',
 };
